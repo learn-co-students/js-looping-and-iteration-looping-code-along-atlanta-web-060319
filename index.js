c71e0f6c0336859cfda850971bad13d38a635c02
github username <github email address>
2019-07-15 18:40:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let array = []
+    for (let i =0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return array;
+};
+
+function countdown(num){
+    let count = num
+    while (count >= 0) {
+        console.log(count);
+       count -= 1 ;
+    }
+}
+        
+
+
